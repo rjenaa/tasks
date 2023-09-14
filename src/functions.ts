a48681bd56db63038312ddfb82d16589d6622345
raymundo <rjen@udel.edu>
2023-09-13 21:36:02 -0400

    Added functionality to the given functions

--- src/functions.ts
+++ src/functions.ts
@@ -4,7 +4,8 @@
  *      C = (F - 32) * 5/9
  */
 export function fahrenheitToCelius(temperature: number): number {
-    return 0;
+    let celius: number = ((temperature - 32) * 5) / 9;
+    return celius;
 }
 
 /**
@@ -12,15 +13,26 @@ export function fahrenheitToCelius(temperature: number): number {
  * if the number is greater than zero.
  */
 export function add3(first: number, second: number, third: number): number {
-    return 0;
+    let sum: number = 0;
+    if (first > 0) {
+        sum += first;
+    }
+    if (second > 0) {
+        sum += second;
+    }
+    if (third > 0) {
+        sum += third;
+    }
+    return sum;
 }
 
 /**
  * Consumes a string and produces the same string in UPPERCASE and with an exclamation
  * mark added to the end.
  */
+//https://www.typescriptlang.org/docs/handbook/2/basic-types.html is where I learned the .tUpperCase()
 export function shout(message: string): string {
-    return "";
+    return `${message.toUpperCase()}` + "!";
 }
 
 /**
@@ -28,7 +40,11 @@ export function shout(message: string): string {
  * mark. Do not use an `if` statement in solving this question.
  */
 export function isQuestion(message: string): boolean {
-    return true;
+    let isQ: boolean;
+    message.substring(message.length - 1) === "?"
+        ? (isQ = true)
+        : (isQ = false);
+    return isQ;
 }
 
 /**
@@ -37,5 +53,14 @@ export function isQuestion(message: string): boolean {
  * upper or lower case), then return `false`. Otherwise, return `null`.
  */
 export function convertYesNo(word: string): boolean | null {
-    return true;
+    let convert: boolean;
+    word = word.toLowerCase();
+    if (word === "yes") {
+        convert = true;
+    } else if (word == "no") {
+        convert = false;
+    } else {
+        convert = null;
+    }
+    return convert;
 }
